fe8f046f40a786dc34dad67636b5c81bb53d6b02
Patrick Tulskie <patricktulskie@gmail.com>
2013-03-17 00:35:19 -0400

    Improvements to the way the focusing is forced.

--- HocusFocus.safariextension/start_script.js
+++ HocusFocus.safariextension/start_script.js
@@ -1,2 +1,20 @@
-document.documentElement.tabIndex = "1";
-document.documentElement.focus();
+// Wrap this all up so we don't accidentally screw with the page's JS.
+var HocusFocus = function() {
+  return {
+    forceFocus: function() {
+      // Normally, we would just set this in the Safari extension itself,
+      // but Safari apparently doesn't consider your active search engine
+      // as a domain.
+      currentURL = (window.location + "");
+      searchEngines = /google\.com|bing\.com|yahoo\.com/;
+
+      if (currentURL.search(searchEngines) > -1) {
+        document.documentElement.tabIndex = "1";
+        document.documentElement.focus();
+      }
+    }
+  }
+}
+
+// Fire away!
+new HocusFocus().forceFocus();
